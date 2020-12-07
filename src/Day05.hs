7eb0d6bae1ab1c555b5db1a800acf582cd77c1af
James Sully <sullyj3@gmail.com>
2020-12-07 18:39:08 +1100

    refactor day 5

--- src/Day05.hs
+++ src/Day05.hs
@@ -7,118 +7,82 @@ module Day05 (
   doDay5
   ) where
 
-import Data.Maybe (fromJust)
-import Text.Printf (printf)
-import Data.Foldable
-import Data.List
-import qualified Data.Map.Strict as M
-import           Data.Map.Strict (Map)
+import           Data.Maybe          (fromJust)
+import           Text.Printf         (printf)
+import           Data.Foldable
+import           Data.List
+import qualified Data.Set            as S
+import           Data.Set            (Set)
+import qualified Data.Map.Strict     as M
+import           Data.Map.Strict     (Map)
 import           Control.Monad
 import           Control.Monad.Extra (whenM)
 
-data Region = Reg { regStart :: Int
-                  , regLen   :: Int } deriving (Show, Eq)
 
-data Side = Front | Back | SLeft | SRight deriving (Show, Eq, Ord)
+type BitString = [Bool]
 
+type Plane = Set Int
+type SeatID = Int
+type Row = Int
+type Col = Int
 
-type Path = [Side]
-
-
-parseSide :: String -> Maybe Side
-parseSide = \case
-  "F" -> Just Front
-  "B" -> Just Back
-  "L" -> Just SLeft
-  "R" -> Just SRight
-  _   -> Nothing
-
-parsePath = traverse (parseSide . (:[]))
-
-
-getSeatId :: Int -> Int -> Int
+getSeatId :: Row -> Col -> SeatID
 getSeatId row col = row * 8 + col
 
+seatRow :: SeatID -> Row
+seatRow = (`div` 8)
 
-bSearch :: Int -> Path -> Int
-bSearch initialSize path = n
-  where
-    Reg n _ = foldl' narrow (Reg 0 initialSize) path
+seatCol :: SeatID -> Col
+seatCol = (`mod` 8)
 
+getRow :: Row -> Set SeatID
+getRow r = S.map (+ (r*8)) $ S.fromList [0..7]
 
-narrow :: Region -> Side -> Region
-narrow (Reg {regStart, regLen}) = \case
-  Front  -> Reg regStart halfLen
-  Back   -> Reg (regStart + halfLen) halfLen
-  SLeft  -> Reg regStart halfLen
-  SRight -> Reg (regStart + halfLen) halfLen
-  where
-    halfLen = regLen `div` 2
+planeRow :: Plane -> Row -> Set SeatID
+planeRow plane r = S.intersection (getRow r) plane
 
-
-getRow :: Path -> Int
-getRow = bSearch 128
+showSeat :: SeatID -> String
+showSeat s = printf "id: %d, row: %d, col: %d" s (seatRow s) (seatCol s)
 
 
-getCol :: Path -> Int
-getCol = bSearch 8
+bitStringInt :: [Bool] -> Int
+bitStringInt bs = let oneZeroes = map (\b -> if b then 1 else 0) bs
+  in sum $ zipWith (\bit pow -> bit*(2^pow))
+                   (reverse $ oneZeroes)
+                   [0..]
 
+
+parseBit :: String -> Maybe Bool
+parseBit = \case
+  "F" -> Just False
+  "B" -> Just True
+  "L" -> Just False
+  "R" -> Just True
+  _   -> Nothing
+
+
+parseBitString :: String -> Maybe BitString
+parseBitString = traverse (parseBit . (:[]))
+
+
+pathToRowCol :: String -> (Int, Int)
+pathToRowCol p = let (rPath, cPath) = splitAt 7 p
+                     (row, col) = ( bitStringInt . fromJust . parseBitString $ rPath
+                                  , bitStringInt . fromJust . parseBitString $ cPath )
+                  in (row, col)
 
 doDay5 :: IO ()
 doDay5 = do
   part1
 
-
+part1 :: IO ()
 part1 = do
   let fp = "inputs/day5.txt"
-  paths <- fromJust . traverse parsePath . lines <$> readFile fp
-
-  theMap <- foldM add mempty paths
-
-  let sids = map (\(_,_,x) -> x) $ M.elems theMap
-  print $ maximum sids
-
-  pure ()
-    where
-      add :: Map Path (Int, Int, Int) -> Path -> IO (Map Path (Int, Int, Int))
-      add acc p = do
-          let matches = M.filter (==val) acc
-          when (matches /= mempty) $ do
-             putStrLn "Trying to insert"
-             print (p, val)
-             putStrLn "but val already exists in the map"
-             print matches
-             error "eek"
+  rowCols <- map pathToRowCol . lines <$> readFile fp
+  let seatIds = uncurry getSeatId <$> rowCols
+  print $ maximum seatIds
 
 
-          pure (M.insert p val acc)
-
-        where (rPath, colPath) = splitAt 7 p
-              r = getRow rPath
-              c = getCol colPath
-              sid = getSeatId r c
-              val = (r,c,sid)
-
-
-
--- --------------
--- Testing
--- --------------
-
-testPart1 :: IO ()
-testPart1 = do
-  for_ testCases checkCase
-  where
-    checkCase :: (String, Int, Int, Int) -> IO ()
-    checkCase testCase@(path, expectedRow, expectedCol, expectedSeatId) = do
-      putStrLn $ "testcase: " <> show testCase
-      printf "result: row - %d, col - %d, id %d\n" row col sId
-      print $ (row, col, sId) == (expectedRow, expectedCol, expectedSeatId)
-      putStrLn "\n"
-      where path' = fromJust $ traverse (parseSide . (:[])) path
-            (rowPath,colPath) = splitAt 7 path'
-            (row, col) = (getRow rowPath, getCol colPath)
-            sId = getSeatId row col
 
 
 
