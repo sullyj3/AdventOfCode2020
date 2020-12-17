e6de89e8822e5937c0776478c1f73490d7c391dc
James Sully <sullyj3@gmail.com>
2020-12-18 05:50:38 +1100

    Remove unused boilerplate from main

--- app/Main.hs
+++ app/Main.hs
@@ -15,7 +15,7 @@ import Day09
 import Day10
 import Day11
 import Day12
--- import Day13
+import Day13
 -- import Day14
 -- import Day15
 -- import Day16
@@ -29,26 +29,7 @@ import Day12
 -- import Day24
 -- import Day25
 
-currentDay = doDay12
+currentDay = doDay13
 
 main :: IO ()
-main = do
-  args <- getArgs
-  case args of
-    [] -> currentDay
-    [dayNumber] -> case readMaybe dayNumber of
-      Just 1 -> doDay1
-      Just 2 -> doDay2
-      Just 3 -> doDay3
-      Just 4 -> doDay4
-      Just 5 -> doDay5
-      Just 6 -> doDay6
-      Just 7 -> doDay7
-      Just 8 -> doDay8
-      Just 9 -> doDay9
-      Just 10 -> doDay10
-      Just 11 -> doDay11
-      Just 12 -> doDay12
-      Just _ -> putStrLn "I haven't implemented that day yet!"
-      Nothing -> putStrLn "That's not a number!"
-    _ -> putStrLn "Please enter a day number!"
+main = currentDay
