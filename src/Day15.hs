725dec47b6ab0c7a77088f5be677102cf5d6f0a0
James Sully <sullyj3@gmail.com>
2020-12-21 18:30:34 +1100

    More cleanup day 15

--- src/Day15.hs
+++ src/Day15.hs
@@ -10,15 +10,15 @@ import           Text.Read (readMaybe)
 import qualified Data.Map as M
 import           Data.Map (Map, (!?))
 
---------------------------
 -------- Parsing ---------
 --------------------------
+
 parse :: String -> Maybe [Integer]
 parse = traverse readMaybe . splitWhen (==',')
 
+------- Solution ---------
 --------------------------
--------- Part 1 ----------
---------------------------
+
 type LastSeen = Map Integer Integer
 data SeqState = SeqState { ssLastSeen :: LastSeen, ssLastN :: Integer, ssLastTurn :: Integer }
 
@@ -36,25 +36,14 @@ theSequence initial = initial ++ unfoldr next initialState
                 newState newVal (SeqState {..}) =
                   SeqState (M.insert ssLastN ssLastTurn ssLastSeen) newVal currTurn
 
-
-part1 :: [Integer] -> Integer
-part1 initial = theSequence initial !! (2020 - 1)
-
---------------------------
--------- Part 2 ----------
---------------------------
-part2 :: [Integer] -> Integer
-part2 initial = theSequence initial !! (30000000 - 1)
-
---------------------------
 ---------- IO ------------
 --------------------------
+
 doDay15 :: IO ()
 doDay15 = do
   -- let fp = "inputs/day15test.txt"
   let fp     = "inputs/day15.txt"
   input <- readFile fp
   let Just ns = parse input
-  print $ part1 ns
-  print $ part2 ns
-
+  print $ theSequence ns !! (2020 - 1)
+  print $ theSequence ns !! (30000000 - 1)
